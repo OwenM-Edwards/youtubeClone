09e3b910328a4b2a3b7af879b6b4354e0dad12ac
Zibbly <owenhedwards@gmail.com>
2020-09-23 23:50:01 +0100

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,8 +7,14 @@ import {
   Switch,
 } from "react-router-dom";
 import { Sidebar, Header } from './components';
-import { Watch, Search } from './containers';
+import { Watch, Search, SignIn, Register, Home } from './containers';
 import StyledContent from './styles/Content';
+
+const theme = {
+   primaryBGColor:'#1f1f1f',
+   secondBGColor:'#181818',
+   primaryFontColor:'#EAEAEA',
+}
 
 
 const Wrapper = styled.section`
@@ -20,28 +26,46 @@ const Wrapper = styled.section`
 `;
 
 
+
 const AppRouter = () => {
    return(
-      <Router>
-         <Wrapper>
-            <Header></Header>
-            <StyledContent> 
-               <Switch>
-                  <Route 
-                     path="/search/:searchstring"
-                     render={() => <Search/>} 
-                  />
-                  <Route 
-                     path="/watch/:videoId"
-                     render={() => <Watch/>} 
-                  />
-                  <Route path="/"/>
-                  <Redirect to="/" />
-               </Switch>
-            </StyledContent>
-            <Sidebar></Sidebar>
-         </Wrapper>
-      </Router>
+      <ThemeProvider theme={theme}>
+         <Router>
+            <Switch>
+               <Route 
+                  path="/signIn"
+                  render={() => <SignIn/>} 
+               />
+               <Route 
+                  path="/register"
+                  render={() => <Register/>} 
+               />
+
+               <Wrapper>
+                  <Header></Header>
+                  <StyledContent> 
+                        <Route 
+                           path="/search/:searchstring"
+                           render={() => <Search/>} 
+                        />
+                        <Route 
+                           path="/watch/:videoId"
+                           render={() => <Watch/>} 
+                        />
+                        <Route 
+                           path="/home/"
+                           render={() => <Home/>} 
+                        />
+                        <Route path="/"/>
+                  </StyledContent>
+                  <Sidebar></Sidebar>
+               </Wrapper>
+
+               <Redirect to="/" />
+            </Switch>
+         </Router>
+      </ThemeProvider>
+
    )
 }
 
