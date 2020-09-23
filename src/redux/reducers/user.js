f348cd3190ffb598f0e53d942f512ae65b017ce0
Zibbly <owenhedwards@gmail.com>
2020-09-23 23:49:55 +0100

    Create user.js

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,19 @@
+const initialState = {
+   authenticated:false,
+};
+
+
+const userReducer = (state = initialState, action) => {
+   switch(action.type){
+      case 'SIGN_IN':
+         return {
+            authenticated:action.payload,
+         }
+      
+      default:
+         return state;
+   }
+}
+
+
+export default userReducer;
