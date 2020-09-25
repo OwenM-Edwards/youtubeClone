ae20e1edea28358f0eb164e62db4dca7f9d6d8a3
Zibbly <owenhedwards@gmail.com>
2020-09-25 20:11:00 +0100

    Update CSS

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
+import { useHistory, useLocation, useRef } from "react-router-dom";
+import {useSelector} from 'react-redux';
 
+
+// Main sidebar to left of page.
+const Sidebar = () => {
+   const history = useHistory();
+   const location = useLocation();
+   const divRef = React.useRef();
+   const sidebarToggleState = useSelector(state=>state.toggleSidebar.isExtended);
+
+   // On sidebar button click, send to relevant page.
+   const handleButtonClick = (target) => {
+      switch(target){
+         case 'home':
+            history.push(`/home`);
+            break;
+         case 'subscriptions':
+            history.push(`/subscriptions`);
+            break;
+         case 'library':
+            history.push(`/library`);
+            break;
+         case 'history':
+            history.push(`/history`);
+            break;
+         default:
+            history.push(`/home`);
+      }
+   }
+
+   // Conditional styling for active button, depending on current app path location.
+   const buttonActiveStyle = { backgroundColor: "#383838", };
+   const buttonInactiveStyle = { backgroundColor: "#1f1f1f", };
+
+   if (sidebarToggleState){
+      return(
+         // Full extended sidebar.
+         <StyledSidebar>
+            <StyledButtonContainer>
+               <StyledButton 
+                     ref={divRef} 
+                     style={location.pathname === '/' || location.pathname === '/home' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('home')}   
+                  >
+                  <StyledButtonImage src={require('../img/home.png')}/>
+                  Home
+               </StyledButton>
+   
+               <StyledButton 
+                     ref={divRef} 
+                     style={location.pathname === '/subscriptions' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('subscriptions')}   
+                  >
+                  <StyledButtonImage src={require('../img/subscription.png')}/>
+                  Subscriptions
+               </StyledButton>
+            </StyledButtonContainer>
+           
+            <StyledButtonContainer>
+               <StyledButton 
+                     ref={divRef} 
+                     style={location.pathname === '/library' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('library')}   
+                  >
+                  <StyledButtonImage src={require('../img/library.png')}/>
+
+                  Libary
+               </StyledButton>
+   
+               <StyledButton 
+                     ref={divRef} 
+                     style={location.pathname === '/history' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('history')}   
+                  >
+                  <StyledButtonImage src={require('../img/history.png')}/>
+                  History
+               </StyledButton>
+            </StyledButtonContainer>
+         </StyledSidebar>
+      )
+   }
+
+   else{
+      return(
+         // Retracted, smaller sidebar.
+         <StyledRetractedSidebar>
+            <StyledRetractedButtonContainer>
+               <StyledRetractedButton 
+                     ref={divRef} 
+                     style={location.pathname === '/' || location.pathname === '/home' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('home')}   
+                  >
+                  <StyledRetractedButtonImage src={require('../img/home.png')}/>
+                  Home
+               </StyledRetractedButton>
+   
+               <StyledRetractedButton 
+                     ref={divRef} 
+                     style={location.pathname === '/subscriptions' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('subscriptions')}   
+                  >
+                  <StyledRetractedButtonImage src={require('../img/subscription.png')}/>
+                  Subscriptions
+               </StyledRetractedButton>
+            </StyledRetractedButtonContainer>
+           
+            <StyledRetractedButtonContainer>
+               <StyledRetractedButton 
+                     ref={divRef} 
+                     style={location.pathname === '/library' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('library')}   
+                  >
+                  <StyledRetractedButtonImage src={require('../img/library.png')}/>
+                  Libary
+               </StyledRetractedButton>
+   
+               <StyledRetractedButton 
+                     ref={divRef} 
+                     style={location.pathname === '/history' ? buttonActiveStyle : buttonInactiveStyle}
+                     onClick={()=>handleButtonClick('history')}   
+                  >
+                  <StyledRetractedButtonImage src={require('../img/history.png')}/>
+                  History
+               </StyledRetractedButton>
+            </StyledRetractedButtonContainer>
+         </StyledRetractedSidebar>
+      )
+   }
+
+}
+
+// Full extended sidebar.
 const StyledSidebar = styled.nav`
    position:fixed;
    height:100%;
-   width:200px;
-   z-index:1;
-   top:5%;
+   width:210px;
    left:0;
    overflow-x:hidden;
    overflow-y:hidden;
@@ -16,12 +146,79 @@ const StyledSidebar = styled.nav`
    &:hover {
       overflow-y:scroll;
    }
+   padding-top:90px;
 `;
+const StyledButton= styled.button`
+   width:100%;
+   height:40px;
+   margin: 5px auto;
+   background-color: #1f1f1f;
+   color:#e3e3e3;
+   border: none;
+   cursor:pointer;
+   text-align:left;
+   padding-left:30px;
+   padding-bottom:5px;
+
+`
+const StyledButtonContainer= styled.div`
+   width:100%;
+   height:auto;
+   margin-bottom:20px;
+   border-bottom:1px solid rgba(255,255,255,0.2);
+
+`
+const StyledButtonImage = styled.img`
+   width:20px;
+   position:relative;
+   right:10px;
+   top:4px;
+`
 
 
-const Sidebar = () => {
-   return(
-      <StyledSidebar>This is the side bar</StyledSidebar>
-   )
-}
+// Retracted, smaller sidebar.
+const StyledRetractedSidebar = styled.nav`
+   position:fixed;
+   height:100%;
+   width:74px;
+   left:0;
+   overflow:hidden;
+   overflow-y:hidden;
+   background-color: #1f1f1f;
+   cursor: pointer;
+   color: #e3e3e3;
+   &:hover {
+      overflow-y:scroll;
+   }
+   padding-top:90px;
+`
+const StyledRetractedButtonContainer= styled.div`
+   width:100%;
+   height:auto;
+   margin-bottom:20px;
+   border-bottom:1px solid rgba(255,255,255,0.2);
+
+`
+const StyledRetractedButton= styled.button`
+   width:100%;
+   height:60px;
+   margin: 15px auto;
+   background-color: #1f1f1f;
+   color:#e3e3e3;
+   border: none;
+   cursor:pointer;
+   text-align:center;
+   font-size:0.6rem;
+   
+
+`
+const StyledRetractedButtonImage = styled.img`
+   width:20px;
+   position:relative;
+   left:20px;
+   bottom:3px;
+   display:block;
+`
+
+
 export default Sidebar
