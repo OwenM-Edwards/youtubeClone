7468134ca8ff9ed9a9eeedf7c7c220cf14e965e1
Zibbly <owenhedwards@gmail.com>
2020-09-25 20:09:15 +0100

    Updates CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,102 @@ import {
    Link,
 } from "react-router-dom";
 import {useSelector,useDispatch} from 'react-redux';
-import { authenticateUser } from '../redux/actions';
-import profileImg from '../img/user.png';
+import { authenticateUser, toggleSidebar } from '../redux/actions';
+import profileImg from '../img/blankProfile.png';
 
+
+
+// Topbar of page, 
+// holds the site logo,
+// login button and status
+// and the main searchbar.
+const Header = () => {
+   const dispatch = useDispatch();
+   const [profileTabState, setProfileTabState] = useState(false);
+   const authenticated = useSelector(state=>state.authenticated.authenticated);
+   const sidebarToggleState = useSelector(state=>state.toggleSidebar.isExtended);
+  
+
+   const loginUser = (state) => {
+      dispatch(authenticateUser(state));
+   }
+
+   // Fired from mobile style hamburger button, extends or retracts the sidebar.
+   const handleToggleSidebar = () => {
+      if (sidebarToggleState === true){
+         dispatch(toggleSidebar(false));
+      }
+      else {
+         dispatch(toggleSidebar(true));
+      }
+   }
+
+   return(
+      <StyledHeader>
+         <StyledMobileBurgerButton onClick={handleToggleSidebar}>
+            <StyledMobileBurgerButtonImg src={require('../img/mobileBurger.png')}/>
+         </StyledMobileBurgerButton>
+         <Link style={{width:"0"}} to="/">
+            <StyledLogo src={require('../img/test.png')}></StyledLogo>
+         </Link>
+         
+         <SearchBar/>
+
+         {authenticated
+            ?  <Link to="/upload" style={{ textDecoration: 'none' }}>
+                  <StyledUploadButton><StyledUploadImage src={require('../img/upload.png')}/></StyledUploadButton>
+               </Link> 
+            :  <Link to="/signin" style={{ textDecoration: 'none' }}>
+                  <StyledUploadButton><StyledUploadImage src={require('../img/upload.png')}/></StyledUploadButton>
+               </Link> 
+         }
+
+         <React.Fragment>
+            {authenticated 
+               ?  <StyledProfileButtonLoggedIn onClick={()=>profileTabState? setProfileTabState(false):setProfileTabState(true) }/>
+               :  <Link to="/signIn" style={{ textDecoration: 'none' }}>
+                     <StyledProfileButtonLoggedOut onClick={()=>profileTabState? setProfileTabState(false):setProfileTabState(true) }>
+                        <StyledProfileImg src={profileImg}/>
+                        <p style={{fontSize:"18px" ,color:"#E3E3E3"}}>SIGN IN</p>
+                     </StyledProfileButtonLoggedOut>
+                  </Link>
+            }
+            {profileTabState 
+               ? authenticated 
+                  ? <StyledProfileTab><h1 onClick={()=>loginUser(false)}>SIGN OUT</h1></StyledProfileTab> 
+                  : <React.Fragment/> 
+               : <React.Fragment/>
+            }
+         </React.Fragment>
+
+
+      </StyledHeader>
+   )
+}
+
+
+
+const StyledUploadButton = styled.button`
+   width:50px;
+   height:33px;
+   cursor:pointer;
+   position:relative;
+   bottom:5px;
+   right:10px;
+
+   outline:none;
+   color:inherit;
+   background:none;
+   border:none;
+`
+const StyledUploadImage = styled.img`
+   width:100%;
+`
 
 const StyledHeader = styled.header`
    flex-shrink: 0;
    width:100%;
-   height:80px;
+   height:60px;
    background-color:${props => props.theme.primaryBGColor};
    display:flex;
    flex-direction:row;
@@ -25,9 +113,25 @@ const StyledHeader = styled.header`
 `
 
 const StyledLogo = styled.img`
-   height:100%;
+   height:170%;
    position:relative;
-   left:30px;
+   left:28px;
+   bottom:15px;
+`
+const StyledMobileBurgerButton = styled.button`
+   outline:none;
+   color:inherit;
+   background:none;
+   margin:0;
+   border:none;
+   position:relative;
+   left:0px;
+   width:40px;
+   bottom:5px;
+   cursor:pointer;
+`
+const StyledMobileBurgerButtonImg = styled.img`
+   height: 90%;
 `
 
 const StyledProfileButtonLoggedIn = styled.button`
@@ -35,8 +139,8 @@ const StyledProfileButtonLoggedIn = styled.button`
    z-index:2;
    border-radius:100%;
    position:relative;
-   width:60px;
-   height:60px;
+   width:40px;
+   height:40px;
    bottom:10px;
    cursor:pointer;
 `
@@ -44,84 +148,47 @@ const StyledProfileButtonLoggedIn = styled.button`
 const StyledProfileButtonLoggedOut = styled.button`
    background-color:${props => props.theme.primaryBGColor};
    color:${props => props.theme.primaryFontColor};
-   border-color:${props => props.theme.primaryFontColor};
    z-index:2;
    position:relative;
-   width:140px;
-   height:60px;
+   width:120px;
+   height:40px;
    bottom:10px;
    cursor:pointer;
    display:flex;
    padding:10px;
    justify-content:center;
    align-items:center;
-   border-radius:5px;
+   border-radius:2px;
    transition: all 0.2s;
+   text-decoration: none;
+   border:1px solid ${props => props.theme.primaryFontColor};
 
-   &:hover {
-      background-color:${props => props.theme.primaryFontColor};
-   }
+   // &:hover {
+   //    background-color:${props => props.theme.primaryFontColor};
+   // }
 
 `
 
 const StyledProfileTab = styled.div`
-   background-color:red;
+   background-color:${props => props.theme.primaryBGColor};
+   border-bottom: 0.6px solid ${props => props.theme.primaryFontColor};
+   border-right: 0.6px solid ${props => props.theme.primaryFontColor};
+   border-left: 0.6px solid ${props => props.theme.primaryFontColor};
    position:relative;
    z-index:-1;
    position:absolute;
    width:140px;
    height:300px;
-   top:70px;
-   right:10px;
+   top:30px;
+   right:80px;
    text-align:center;
 `
 const StyledProfileImg = styled.img`
-   height:80%;
+   height:120%;
    padding-right:10px;
 
 `
 
-
-const Header = () => {
-   const dispatch = useDispatch();
-   const [profileTabState, setProfileTabState] = useState(false);
-   const authenticated = useSelector(state=>state.authenticated.authenticated);
-   console.log(authenticated);
-
-   const loginUser = (state) => {
-      dispatch(authenticateUser(state));
-   }
-
-   return(
-      <StyledHeader>
-         
-         <Link style={{width:"0"}} to="/">
-            <StyledLogo src={require('../img/test.png')}></StyledLogo>
-         </Link>
-         
-         <SearchBar/>
-
-         <React.Fragment>
-            {authenticated 
-               ? <StyledProfileButtonLoggedIn onClick={()=>profileTabState? setProfileTabState(false):setProfileTabState(true) }/>
-               : <Link to="/signIn">
-                     <StyledProfileButtonLoggedOut onClick={()=>profileTabState? setProfileTabState(false):setProfileTabState(true) }>
-                        <StyledProfileImg src={profileImg}/>
-                        <p style={{fontSize:"18px",color:"#E3E3E3"}}>SIGN IN</p>
-                     </StyledProfileButtonLoggedOut>
-                  </Link>
-            }
-            {profileTabState 
-               ? authenticated 
-                  ? <StyledProfileTab><h1 onClick={()=>loginUser(false)}>SIGN OUT</h1></StyledProfileTab> 
-                  : <React.Fragment/> 
-               : <React.Fragment/>
-            }
-         </React.Fragment>
-         
-      </StyledHeader>
-   )
-}
 
 
 export default Header
