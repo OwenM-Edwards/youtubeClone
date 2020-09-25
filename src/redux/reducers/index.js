aaa938bf217d92fb4a4960571b0621c1a0793107
Zibbly <owenhedwards@gmail.com>
2020-09-25 20:11:42 +0100

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,16 @@
 import setSearchReducer from './setSearch';
 import setSearchByIDReducer from './setSearchByID';
 import userReducer from './user';
+import toggleSidebarReducer from './toggleSidebar';
 import {combineReducers} from 'redux';
-
+import userStoreReducer from './userStore';
 
 const rootReducer = combineReducers({ 
    searchQuery:setSearchReducer,
    selectedVideoID:setSearchByIDReducer,
    authenticated:userReducer,
+   toggleSidebar:toggleSidebarReducer,
+   userStore:userStoreReducer,
 })
 
 
