ba72b62c32ce85de192e5f2723f8760ba4b6b074
Zibbly <owenhedwards@gmail.com>
2020-09-25 20:08:58 +0100

    Add user login check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './Router'
-
+import {useDispatch} from 'react-redux';
+import { authenticateUser } from './redux/actions';
 
 const App = () => {
+   const dispatch = useDispatch();
+   useEffect(() => {
+      const isLoggedInUser = localStorage.getItem("user");
+      if (isLoggedInUser === false ) {
+         dispatch(authenticateUser(true));
+      }
+      else {
+         dispatch(authenticateUser(false));
+      }
+   })
    return (
       <Router></Router>
    )
